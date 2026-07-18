aad0e892366f1b177453060dab9bbbce20c0b588
koichi <koichi@51gram.com>
2026-07-18 16:42:09 +0900

    Remove unused legacy Works section code
    Delete the old Works/WorkCard/ProjectModal components and their
    data files, plus the corresponding .work-* CSS -- none of this has
    been referenced since the Design Pieces / Motion & Edit /
    Infographics restructure.

--- src/data/workLayout.js
+++
@@ -1,8 +0,0 @@
-export const workLayout = [
-  { top: '0%', left: '2%', width: '38%', rotate: -3, z: 3, speed: 180 },
-  { top: '8%', left: '46%', width: '32%', rotate: 2, z: 2, speed: -150 },
-  { top: '38%', left: '70%', width: '27%', rotate: -4, z: 4, speed: 220 },
-  { top: '54%', left: '6%', width: '34%', rotate: 3, z: 1, speed: -190 },
-  { top: '70%', left: '42%', width: '29%', rotate: -2, z: 3, speed: 160 },
-  { top: '92%', left: '20%', width: '32%', rotate: 4, z: 2, speed: -170 },
-]

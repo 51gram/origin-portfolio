17ab4164f065099673ebfff96b9dc99cffebfcc0
koichi <koichi@51gram.com>
2026-07-22 00:10:37 +0900

    Reposition overlay decorations to updated page coordinates

--- src/components/PageOverlays.jsx
+++ src/components/PageOverlays.jsx
@@ -3,21 +3,21 @@ import scrollDown from '../assets/icons/ScrollDown.svg'
 import bgArrow from '../assets/icons/BgArrow01.svg'
 import logoMarkIcon from '../assets/icons/LogoMarkIcon01.svg'
 
-const scrollDownIcon = { src: scrollDown, x: 725, y: 880, width: 89 }
+const scrollDownIcon = { src: scrollDown, x: 725, y: 780, width: 89 }
 
 const bgArrows = [
-  { x: 760, y: 830 },
-  { x: 715, y: 3450 },
-  { x: 715, y: 5525 },
+  { x: 760, y: 730 },
+  { x: 715, y: 3350 },
+  { x: 715, y: 5425 },
 ]
 
 const logoMarks = [
-  { x: 1315, y: 3470 },
-  { x: 1340, y: 3470 },
-  { x: 60, y: 3555 },
-  { x: 95, y: 3555 },
-  { x: 1315, y: 5510 },
-  { x: 1350, y: 5510 },
+  { x: 1315, y: 3370 },
+  { x: 1350, y: 3370 },
+  { x: 60, y: 3455 },
+  { x: 95, y: 3455 },
+  { x: 1315, y: 5410 },
+  { x: 1350, y: 5410 },
 ]
 
 function PageOverlays() {
